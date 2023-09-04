35de755112a8a30b707c1baec4dfe3183b3fc725
Joshua <Joshua.soto@live.com>
2023-09-04 17:45:19 -0500

    added 5 vars and a function

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 // Assignment code here
+var generateBtn = document.querySelector("#generate");
+function generatePassword() {
 
+var uppercase = confirm("Your password should have an uppercase letter! Click OK to continue");
+var lowercase = confirm("Your password should have a lowercase letter! Click OK to continue");  
+var symbols = confirm("Your password should have a symbol! Click OK to continue");
+var numbers = confirm("Your password should have a number! Click OK to continue");
+var Keylength = prompt("Password must be between 8 and 128 Click OK to continue");
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
