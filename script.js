2f40f60e00d8bfeab0e598b540fc71b9def45509
Joshua <Joshua.soto@live.com>
2023-09-06 12:16:52 -0500

    deleted mistake in javascript

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 var generateBtn = document.querySelector("#generate");
 
 //creates function called generatePassword()
-function generatePassword() 
+function generatePassword() {
 
 var uppercase = confirm("Your password should have an uppercase letter! Click OK to continue");
 var lowercase = confirm("Your password should have a lowercase letter! Click OK to continue");  
@@ -18,26 +18,14 @@ var numeric = "0123456789";
 var multiSelect = [];
 
 //criteria check for the user
-if (keyLength < 8 || keyLength > 128) {
-  alert("Your passwords does not meet the criteria");
-  var keyLength = prompt("Passwords must be between 8 and 128 characters in length!");
-}
-
-if(uppercaseABC === false && lowercaseABC === false specialSymbols === false && numeric === false) {
-return "Your passwords does not meet the password criteria";
-};
-
-var uppercaseABC = confirm("Your password should have an uppercase letter!");
-var lowercaseABC = confirm("Your password should have a lowercase letter!");
-var symbols = confirm("Your password should have a symbol!");
-var numbers = confirm("Your password should have a number!");
-
-
-
 
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+}
+
+
+
 
 // Write password to the #password input
 function writePassword() {
